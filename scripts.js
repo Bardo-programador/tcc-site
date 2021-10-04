8364b737fbd1ca973742b5fd851e6c0ae7784324
Samuel <66800900+samucadeus23@users.noreply.github.com>
2021-10-04 16:17:01 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+window.alert('testando')
+$(document).ready(function(){
+	$('#select').on('change',function(){
+		var selectValor = '#'+$(this).val();
+		$('#pai').children('div').hide();
+		$('#pai').children(selectValor).show();
+	});
+});
